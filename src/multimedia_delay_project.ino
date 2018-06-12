430e9b272f6f34ef16439c35f420b45a42c31799
Unknown <ingunn.vallestad@gmail.com>
2018-06-12 15:37:57 +0200

    Main project file

---
+++ src/multimedia_delay_project.ino
@@ -0,0 +1,27 @@
+#include "measure_led.h"
+#include "signal_generator_timer.h"
+#include "signal_generator_led.h"
+#include "signal_generator_speaker.h"
+#include "Timer3/Timer3.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  //measureLEDSetup();
+  SGTimerSetup();
+  SGLEDSetup();
+  SGSpeakerSetup();
+  //startSlowCountingTimer3();
+  Serial.begin(9600);
+  while(!Serial);
+  Serial.println("Hello Yún!");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (SGTimerCheckFlag()){
+    SGLEDToggle();
+    SGSpeakerPlaySound();
+  }
+  //printSensorValue();
+  
+}
