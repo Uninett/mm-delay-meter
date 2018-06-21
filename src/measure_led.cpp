efb5d00ab2f4f1892d21f3febf2997db8b996790
ingunnjv <ingunn.vallestad@gmail.com>
2018-06-21 12:43:55 +0200

    Implement measurement sampling with timer3

--- src/measure_led.cpp
+++ src/measure_led.cpp
@@ -4,13 +4,13 @@
 
 #include "Arduino.h"
 #include "measure_led.h"
-#include "Timer3/Timer3.h"
 #include "signal_generator_led.h"
 #include "config.h"
 
 volatile bool led_flag;
 volatile unsigned short delta;
 volatile unsigned long deltaMicros;
+volatile bool sample_flag;
 unsigned long num_runs;
 
 unsigned short samples[NUM_SAMPLES];
@@ -201,15 +201,184 @@ void measureLEDRisingEdgeDetection()
 }
 
 // bool measureLEDCompare(const int* max_samples, const uint8_t last_check_index, const uint8_t first_check_index)
+
+/* Sampling timer and sampling routine */
+
+uint8_t __timer3Control;
+uint16_t __timer3CounterValue;
+
+// On 16 MHz Arduino boards, this function has a resolution of 4us
+// On 8 MHz Arduino boards, this function has a resolution of 8us
+void startTimer3(void) {
+	pauseTimer3();
+	TCCR3A = 0;
+	TCCR3C = 0;
+	#if (F_CPU == 16000000L) || (F_CPU == 8000000L)
+		// CTC mode, prescaler = 64
+		__timer3Control = B00001011;
+		__timer3CounterValue = 0;
+	#else
+		#error("Unsupported CPU frequency")
+	#endif
+	resetTimer3();
+	TIFR3 = 0;
+	// Enable compare match 3A interrupt
+	TIMSK3 = B00000010;
+	// Match at TCNT3 = 4
+	OCR3A = 35;
+	resumeTimer3();
+}
+uint16_t readTimer3(void) {
+	// 17.3 Accessing 16-bit Registers (page 138)
+	uint8_t sreg;
+	uint16_t i;
+	// Save global interrupt flag
+	// 7.4.1 SREG – AVR Status Register (page 14)
+	sreg = SREG;
+	// Disable interrupts
+	cli();
+	// Read TCNTn
+	i = readTimer3Unsafe();
+	// Restore global interrupt flag
+	SREG = sreg;
+	return i;
+}
+void resetTimer3(void) {
+	// 17.3 Accessing 16-bit Registers (page 138)
+	uint8_t sreg;
+	// Save global interrupt flag
+	// 7.4.1 SREG – AVR Status Register (page 14)
+	sreg = SREG;
+	// Disable interrupts
+	cli();
+	// Write TCNTn
+	resetTimer3Unsafe();
+	// Restore global interrupt flag
+	SREG = sreg;
+}
+
+
+
+void samplingSetup()
+{
+	startTimer3();
+	pauseTimer3();
+	sample_flag = 0;
+}
+
+
+ISR(TIMER3_COMPA_vect)
+{
+	// Handle sampling
+	measureLEDRisingEdgeDetection();
+}
+
+bool samplingCheckFlag()
+{
+	if (sample_flag){
+		sample_flag = 0;
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// void measureLEDSetup()
 // {
-// 	for (uint8_t i = last_check_index-1; i >= first_check_index; i--){
-// 		if (max_samples[i] <= max_samples[i-1]){
-// 			// Not strictly increasing
-// 			return false;
-// 		}
-// 		if (max_samples[last_check_index] - max_samples[i] > 10){
-			
-// 		}
-// 	}
+//     // Timer 3 for measuring delay, 64us resolution
+//     startUltraSlowCountingTimer3();
+//     pauseTimer3();
+
+//     measured_delay_flag = 0;
+//     pinMode(lightSensorInterruptPin, OUTPUT);	// Can generate software interrupt
+//     digitalWrite(lightSensorInterruptPin, LOW);
+//     attachInterrupt(digitalPinToInterrupt(lightSensorInterruptPin), measureLEDDelay, RISING);
+
+//     measureLEDInitializeSamples();
+//     num_runs = 0;
 // }
 
+// /* Interrupt service routine for external interrupt at pin 3, triggers when light is recieved at sensor */
+// void measureLEDDelay()
+// {
+//     // Stop timer
+//     pauseTimer3();
+
+//     // Save delay measurement
+
+//     // readTimer3() returns a maximum value of 65535
+//     // That means the maximum possible delta one can measure with this
+//     // function (when in ultra slow counting mode) is 4194ms on 16 MHz boards
+//     delta = readTimer3();
+
+//     // If you estimate deltaMicros could be > 65 ms, or 65535 us,
+//     // delta should be cast to unsigned long, and deltaMicros should be
+//     // created as an unsigned long variable
+//     deltaMicros = microsFromUltraSlowCounting((unsigned long)delta);
+
+//     measured_delay_flag = 1;
+// }
+
+
+
+
+// debug only
+// void printSensorValue(){
+//     // Read analog value (for "debug")
+//     unsigned short raw_sample = analogRead(lightSensorPin);
+//     Serial.print("Light sensor pin: ");
+//     Serial.println(raw_sample);
+// }
+// void printDelay(){
+// 	Serial.print("Light delay: ");
+// 	Serial.print(deltaMicros);
+// 	Serial.println(" us");
+// }
+//
--- src/measure_led.h
+++ src/measure_led.h
@@ -5,10 +5,27 @@ void measureLEDInitializeSamples();
 unsigned short measureLEDMaxSmoothingFilter();
 void measureLEDRisingEdgeDetection();
 
-void measureLEDSetup();
-void measureLEDDelay();
-//void printSensorValue();
-//void printDelay();
 bool measureLEDCheckFlag();
 void measureLEDPrintToSerial();
 double measureLEDGetDelayMs();
+
+
+extern uint8_t __timer3Control;
+extern uint16_t __timer3CounterValue;
+#define readTimer3Unsafe() TCNT3
+#define resetTimer3Unsafe() TCNT3 = __timer3CounterValue
+#define pauseTimer3() TCCR3B = 0
+#define resumeTimer3() TCCR3B = __timer3Control
+extern void startTimer3(void);
+extern uint16_t readTimer3(void);
+extern void resetTimer3(void);
+
+void samplingSetup();
+bool samplingCheckFlag();
+
+
+
+// void measureLEDSetup();
+// void measureLEDDelay();
+//void printSensorValue();
+//void printDelay();
