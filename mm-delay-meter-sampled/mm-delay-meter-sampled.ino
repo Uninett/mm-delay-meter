9b677002b1efbdb12d65451fb92ca3dd9e2a5c83
ingunnjv <ingunn.vallestad@gmail.com>
2018-06-26 16:03:26 +0200

    Implement microphone calibration
    Find idle level

--- mm-delay-meter-sampled/mm-delay-meter-sampled.ino
+++ mm-delay-meter-sampled/mm-delay-meter-sampled.ino
@@ -24,9 +24,53 @@ void setup() {
   signalGeneratorSetup(mode);
   measurementSamplesSetup();
   
+  
+
+  // Calibrate microphone measurements
+  Serial.println("Calibrating microphone...");
+  if(mode == SOUND_MODE){
+    int prev_mic;
+    int current_mic = analogRead(microphonePin);
+    prev_mic = current_mic;
+    int mean = 0;
+    int mic[10];
+    int idle_mic;
+    int idle_iterations = 0;
+    while (idle_iterations < 10){
+      current_mic = analogRead(microphonePin);
+      if (abs(current_mic - prev_mic) < 5){
+        Serial.print(idle_iterations);
+        Serial.print("\t");
+        Serial.print(prev_mic);
+        Serial.print("\t");
+        Serial.println(current_mic);
+        mic[idle_iterations] = current_mic;
+        idle_iterations++;
+      }
+      else{
+        Serial.println("Reset");
+        for (auto &m: mic){
+          m = 0;
+        }
+        idle_iterations = 0;
+      }
+      prev_mic = current_mic;
+    }
+
+    idle_mic = current_mic;
+    for (auto &m: mic){
+      mean += m;
+    }
+    mean = mean/10;    
+    setIdleMicVal(mean);
+    Serial.print("Mean: ");
+    Serial.println(mean);
+  }
+
   resumeTimer1();
   resumeTimer3();
   num_measurements = 0;
+  
 }
 
 void loop() {
