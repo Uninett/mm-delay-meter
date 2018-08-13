e5c6565d81d283362582fb43c2e257fbf224c7d7
ingunnjv <ingunn.vallestad@gmail.com>
2018-08-13 11:57:26 +0200

    Upload to database with correct mediatype

--- shell_scripts/upload.sh
+++ shell_scripts/upload.sh
@@ -10,13 +10,15 @@ CURL_RETURN_CODE=""
 for file in /mnt/sda1/arduino/delay/A_NU.*; do
 	[ -e "$file" ] || continue
 	cat $file
+	# Check if the number of lines is less than a full measurement series
 	if [ $(wc -l $file | cut -f1 -d" ") -lt "5" ]
 	then
 		STATUS="AVBRUTT"
 	else
 		STATUS="MIDLERTIDIG"
 	fi
-	CURL_OUTPUT=$(curl -sSf --data "nokkel=$MAC" --data "status=$STATUS" --data "savetodb=ja&serieno=0" --data "mediatype=Video" \
+	# Upload to database and get status indicators
+	CURL_OUTPUT=$(curl -sSf --data "nokkel=$MAC" --data "status=$STATUS" --data "savetodb=ja&serieno=0" --data "mediatype=Audio" \
 		--data "metode=Uninett-fmaler-v1" --data-urlencode seriedata@$file http://delay.uninett.no/fmaling/dbm.php) 
 	CURL_RETURN_CODE=$?
 	SUBSTRING=$(echo $CURL_OUTPUT | cut -d'>' -f 2 | cut -d'.' -f 1)
@@ -25,7 +27,7 @@ for file in /mnt/sda1/arduino/delay/A_NU.*; do
 	then
 		echo "Success!"
 		new_file=${file/_NU/_U}
-		mv $file $new_file		# Rename to indicate it was uploaded
+		mv $file $new_file	# Rename to indicate the series was uploaded
 		touch $new_file		# Update last-modified date
 	else
 		echo "Insert failed."
@@ -49,7 +51,7 @@ for file in /mnt/sda1/arduino/delay/V_NU.*; do
 	then
 		echo "Success!"
 		new_file=${file/_NU/_U}
-		mv $file $new_file		# Rename to indicate it was uploaded
+		mv $file $new_file	# Rename to indicate the series was uploaded
 		touch $new_file		# Update last-modified date
 	else
 		echo "Upload failed."
