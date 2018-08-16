900a4e05217bcb5f187effd0035ee2aa1da0424f
ingunnjv <ingunn.vallestad@gmail.com>
2018-08-16 15:32:50 +0200

    Configure wifi: connect to eduroam with certificate

--- shell_scripts/wifi_connect.sh
+++ shell_scripts/wifi_connect.sh
@@ -8,7 +8,8 @@ IP_STATUS=0
 
 wifi_status() {
 	SIGNAL=$(/usr/bin/pretty-wifi-info.lua | grep "Signal" | cut -f2 -d" " | cut -f1 -d"%")
-	if [ "$SIGNAL" != "0" -a -n "$SIGNAL" ]
+	MODE=$(/usr/bin/pretty-wifi-info.lua | grep "Mode" | cut -f2 -d" ")
+	if [ "$SIGNAL" != "0" -a -n "$SIGNAL" -a "$MODE" = "Client" ]
 	then
 		IP=$(/usr/bin/pretty-wifi-info.lua | grep "IP address" | cut -f2 -d":" | cut -f1 -d"/")
 		SSID=$(/usr/bin/pretty-wifi-info.lua | grep "SSID" | cut -f2 -d":")
@@ -26,8 +27,8 @@ wifi_status() {
 	fi
 }
 
-wifi_config() {
-	blink-start 100				# Start the blue LED blink
+wifi_config_1() {
+	blink-start 100					# Start the blue LED blink
 	
 	hostname Arduino				# Change the current hostname
 	uci set system.@system[0].hostname='Arduino'
@@ -37,7 +38,9 @@ wifi_config() {
 	uci set wireless.@wifi-iface[0].encryption='none'
 	uci set wireless.@wifi-iface[0].mode='sta'
 	uci set wireless.@wifi-iface[0].ssid='UNINETT_guest'
-	uci set wireless.@wifi-iface[0].key=''
+	uci set wireless.@wifi-iface[0].password=''
+	uci set wireless.@wifi-iface[0].identity=''
+	uci set wireless.@wifi-iface[0].eap_type=''
 	uci set wireless.radio0.channel='auto'
 	uci set wireless.radio0.country='NO'
 	uci delete network.lan.ipaddr
@@ -51,6 +54,36 @@ wifi_config() {
 	wifi
 }
 
+wifi_config_2() {
+	blink-start 100					# Start the blue LED blink
+	
+	hostname Arduino				# Change the current hostname
+	uci set system.@system[0].hostname='Arduino'
+	uci set arduino.@arduino[0].access_point_wifi_name='YUNArduinoAP'
+	
+	# This block resets the wifi password
+	# Makes changes to /etc/config/wireless and /etc/config/network
+	uci set wireless.@wifi-iface[0].encryption='mixed-wpa+aes'
+	uci set wireless.@wifi-iface[0].mode='sta'
+	uci set wireless.@wifi-iface[0].ssid='eduroam'
+	uci set wireless.@wifi-iface[0].client_cert='/etc/config/8a1a1133-76ab-498f-a12b-735b77fa7973.crt.pem'
+	uci set wireless.@wifi-iface[0].priv_key='/etc/config/8a1a1133-76ab-498f-a12b-735b77fa7973.key.pem'
+	uci set wireless.@wifi-iface[0].identity='8a1a1133-76ab-498f-a12b-735b77fa7973@demo.eduroam.no'
+	uci set wireless.@wifi-iface[0].eap_type='tls'
+	uci set wireless.radio0.channel='auto'
+	uci set wireless.radio0.country='NO'
+	uci delete network.lan.ipaddr
+	uci delete network.lan.netmask
+	uci set network.lan.proto='dhcp'
+	
+	# Change the password of the device
+	echo -e "uninett\nuninett" | passwd root
+	uci commit
+	blink-stop
+	wifi
+
+}
+
 # Check initial status
 wifi_status
 
@@ -58,7 +91,7 @@ if [ "$CONNECTED" = "0" ]
 then
 	# Configure wifi and wait for connection
 
-	wifi_config
+	wifi_config_2
 	/bin/sleep 1
 	wifi_status
 	while [ "$CONNECTED" = "0" ]
--- shell_scripts/wifi_status.sh
+++ shell_scripts/wifi_status.sh
@@ -7,7 +7,8 @@ IP_STATUS=0
 
 wifi_status() {
 	SIGNAL=$(/usr/bin/pretty-wifi-info.lua | grep "Signal" | cut -f2 -d" " | cut -f1 -d"%")
-	if [ "$SIGNAL" != "0" -a -n "$SIGNAL" ]
+	MODE=$(/usr/bin/pretty-wifi-info.lua | grep "Mode" | cut -f2 -d" ")
+	if [ "$SIGNAL" != "0" -a -n "$SIGNAL" -a "$MODE" = "Client" ]
 	then
 		IP=$(/usr/bin/pretty-wifi-info.lua | grep "IP address" | cut -f2 -d":" | cut -f1 -d"/")
 		CONNECTED=1
